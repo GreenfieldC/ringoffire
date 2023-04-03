9028bd39d9068e8918fa5f9f469881b34ad48a1b
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-04-03 14:20:03 +0200

    mobile players component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
+import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
+/* import { AppPlayerMobileComponent } from './app-player-mobile/app-player-mobile.component'; */
 
 @NgModule({
 	declarations: [
@@ -31,6 +33,8 @@ import { AngularFireModule } from '@angular/fire/compat';
 		PlayerComponent,
 		DialogAddPlayerComponent,
 		GameTaskComponent,
+  PlayerMobileComponent,
+		/* AppPlayerMobileComponent, */
 	],
 	imports: [
 		BrowserModule,
